8d49864ad64b6391a6f0f36e936548f81219bbea
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-07-03 15:34:50 +1000

    made changes

--- testing/update-plugin.ps1
+++ testing/update-plugin.ps1
@@ -1,3 +1,11 @@
+echo "from rcon.source import Client
+with Client('127.0.0.1', 25575, passwd='password') as client:
+    resp = client.run('pluginmanager unload villagerpedia')" | py
+
 install-module -name Recycle
 Remove-ItemSafely .\plugins\Villagerpedia*.jar
 copy ..\villagerpedia\target\*.jar .\plugins\
+
+echo "from rcon.source import Client
+with Client('127.0.0.1', 25575, passwd='password') as client:
+    resp = client.run('pluginmanager load villagerpedia')" | py
--- villagerpedia/src/main/java/net/maxstuff/villagerpedia/AddToEntitlements.java
+++ villagerpedia/src/main/java/net/maxstuff/villagerpedia/AddToEntitlements.java
@@ -2,6 +2,7 @@ package net.maxstuff.villagerpedia;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Villager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -13,7 +14,9 @@ public class AddToEntitlements implements Listener {
       // Bukkit.broadcastMessage(event.getRightClicked().toString());
       if (event.getRightClicked().toString() == "CraftVillager") {
          //Alert only the player they clicked a villager
-         event.getPlayer().sendMessage("You clicked a Villager");
+         Player player = event.getPlayer();
+         player.sendMessage("You clicked a Villager");
+         
       }
    }
 }
